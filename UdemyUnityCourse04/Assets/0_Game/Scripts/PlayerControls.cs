5420140449ad4514c4f26a81d6a0025b5336f9db
JohnPantsWork <JohnPantsWork@gmail.com>
2021-08-12 00:18:20 +0800

    [Aming] folder organize

--- UdemyUnityCourse04/Assets/0_Game/Scripts/PlayerControls.cs
+++ UdemyUnityCourse04/Assets/0_Game/Scripts/PlayerControls.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float xThrow = Input.GetAxis("Horizontal");
+        Debug.Log(xThrow);
+
+        float yThrow = Input.GetAxis("Vertical");
+        Debug.Log(yThrow);
     }
 }
