72e57bdee6fc8f453acfbbe70ee7c87aeeac9d9d
JohnPantsWork <JohnPantsWork@gmail.com>
2021-08-15 21:40:00 +0800

    [Aming] SFX、VFX、MonsterWavePattern、Post-Processing all in final update

--- UdemyUnityCourse04/Assets/0_Game/Scripts/Enemy.cs
+++ UdemyUnityCourse04/Assets/0_Game/Scripts/Enemy.cs
@@ -4,32 +4,44 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
+    [SerializeField] GameObject deathFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] int scorePerHit = 15;
+    [SerializeField] int scorePerKill = 150;
+    [SerializeField] int hitpoints = 2;
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        gameObject.AddComponent<Rigidbody>().useGravity = false;
+        parentGameObject = GameObject.FindWithTag("SpawnARuntime");
     }
 
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if (hitpoints < 1)
+        {
+            KillEnemy();
+        }
     }
 
     void ProcessHit()
     {
         scoreBoard.IncreaseScore(scorePerHit);
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        hitpoints--;
     }
 
     void KillEnemy()
     {
-        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        scoreBoard.IncreaseScore(scorePerKill);
+        GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
         GameObject.Destroy(this.gameObject);
     }
 }
